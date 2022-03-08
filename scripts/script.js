7ac7a8a64170499aedf14eeb220461917993a923
Fedko Zhenja <fedkozhenja@gmail.com>
2022-03-08 21:58:55 +0300

    Adding styles

--- scripts/script.js
+++ scripts/script.js
@@ -9,25 +9,37 @@
 const root = document.getElementById('root');
 
 const toDoList = document.createElement("div");
+toDoList.classList.add("container");
 
 const input = document.createElement("input");
 input.placeholder = "ToDo";
+input.classList.add("input");
 
 function onAddButtonClick() {
-    console.log(input.value)
     const newToDo = document.createElement("li");
-    newToDo.innerText = input.value;
-    textList.append(newToDo); 
+    
+    if (input.value == "" ) {
+        alert ("Вы ничего не ввели!")
+    } else {
+        newToDo.innerText = input.value;
+        input.value = "";
+        textList.append(newToDo); 
+    }
 }
 
 const addButton = document.createElement("button");
 addButton.innerText = "Add";
-addButton.onclick = onAddButtonClick
+addButton.onclick = onAddButtonClick;
+addButton.classList.add("add_Button");
+
+
 
 const titleList = document.createElement("p");
 titleList.innerText = "List";
 
 const textList = document.createElement("ul");
+textList.classList.add("text_List");
+
 const firstToDo = document.createElement("li");
 firstToDo.innerText = "First To Do"
 textList.append(firstToDo);
